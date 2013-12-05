b1b64d38acaa5a78d702e45920ba6c01da55dd11
Alex Gru <agruschi@cosy.sbg.ac.at>
2013-12-05 14:04:52 +0100

    added performance test routine, actual tests need to be done tough

--- Assignment-4/src/dbt04/Main.java
+++ Assignment-4/src/dbt04/Main.java
@@ -10,6 +10,26 @@ import java.sql.SQLException;
 public class Main {
     public static final int TOTALNUMBEROFLINES = 1233216;
     public static final int NUMBERLINESSPLIT = 10000;
+    private static final String notExistingValue = "j31j0fj9u309u5r";
+    private static final String[] pubidConds = new String[]{"books/eh/campbell84/CampbellH84",
+            "books/idea/encyclopedia2005/GelmanTFC05", "books/idea/encyclopedia2005/PotgieterAB05",
+            "books/idea/encyclopedia2005/VerburgAR05", "conf/mm/Li02", "conf/normas/Kibble05",
+            "journals/corr/cs-DB-0306013", "journals/gpem/Banzhaf03", "journals/kbs/NakakojiYO00",
+            "journals/pami/KirbyS90", notExistingValue, "journals/csda/CoppiD06", "journals/ijstm/GormanS00",
+            "journals/endm/AcharyaJ03", "series/vdi/Bogel07"};
+    private static final String[] booktitleConds = new String[]{"Modern Database Systems",
+            "Specification and Verification of Concurrent Systems", notExistingValue, "Temporal Databases",
+            "The Computer Science and Engineering Handbook",
+            "Tree Automata and Languages", "Encyclopedia of Information Science and Technology (IV)",
+            "Web & Datenbanken", "ACSD", "The Industrial Information Technology Handbook", "The Compiler Design Handbook",
+            "Object-Oriented Concepts, Databases, and Applications", "On the Construction of Programs",
+            "Web Engineering: Systematische Entwicklung von Web-Anwendungen", "Implementations of Prolog",
+            "Logic Programming: Formal Methods and Practical Applications"};
+    private static final String[] yearConds = new String[]{"1995", "1990", "2005", "1984", "2000", "1980", "2002", "1986",
+            "1997", "2003", "1987", "2001", "1983", "1988", "1992"};
+    private static final String[][] conds = new String[][]{pubidConds, booktitleConds, yearConds};
+    private static final String[] attributes = new String[]{"pubID", "booktitle", "year"};
+    private static final String[] independentAttributes = new String[]{"type", "year", "title"};
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
         final Connection connection = DBConnector.getConnection();
@@ -17,6 +37,46 @@ public class Main {
 //        addPublTable(connection);
 //        File[] files = splitFileIntoMore("publ.tsv", NUMBERLINESSPLIT);
 //        DBFiller.fillDB(connection, files);
+        testQueryPerformance(connection, 10, "hash", false);
+    }
+
+    private static void testQueryPerformance(Connection conn, int numberRunsPerQuery, String dataStructure,
+                                             boolean clustered) throws SQLException {
+        System.out.println("----- QUERY PERFORMANCE TEST -----");
+        double totalTime = 0;
+
+        for (int i = 0; i < conds.length; i++) {
+            String currAttribute = attributes[i];
+            conn.createStatement().execute("DROP INDEX IF EXISTS " + currAttribute + "_idx");
+            String index = "CREATE INDEX " + currAttribute + "_idx " + "ON publ USING " + dataStructure + "(" +
+                    currAttribute + ");";
+            String cluster = null;
+            if (clustered) {
+                cluster = "ALTER TABLE publ CLUSTER ON " + currAttribute + "_idx";
+            } else {
+                conn.createStatement().execute("DROP INDEX IF EXISTS " + independentAttributes[i] + "_idx");
+                conn.createStatement().execute("CREATE INDEX " + independentAttributes[i] + "_idx ON publ USING "
+                        + dataStructure + " (" + currAttribute + ")");
+                cluster = "ALTER TABLE publ CLUSTER ON " + independentAttributes[i] + "_idx";
+            }
+            System.out.println("\t" + index);
+            System.out.println("\t" + cluster);
+            conn.createStatement().execute(index);
+            conn.createStatement().execute(cluster);
+
+            long start = System.nanoTime();
+            for (int j = 0; j < numberRunsPerQuery; j++) {
+                String query = "SELECT * FROM Publ WHERE " + currAttribute + "='" + conds[i][j] + "'";
+                System.out.println(query);
+                conn.createStatement().executeQuery(query);
+            }
+            long finish = System.nanoTime();
+            double time = (finish - start) / Math.pow(10, 9);
+            totalTime += time;
+            System.out.println("Query took " + time + " seconds.");
+        }
+        System.out.println("Total time [cluster: " + clustered + ", " + dataStructure + "]: " + totalTime + " seconds.");
+
     }
 
     private static File[] splitFileIntoMore(String fileName, int maxNumberOfRows) throws IOException {
